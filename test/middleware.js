0c240c8e76bf0d280a60b5ef21fa30fc5fa71b1e
Chris O'Hara <cohara87@gmail.com>
2013-12-12 19:14:08 +1100

    Expose the view helper for defining assets

--- test/middleware.js
+++ test/middleware.js
@@ -2,6 +2,7 @@ var assert = require('assert')
   , path = require('path')
   , http = require('http')
   , express = require('express')
+  , nunjucks = require('nunjucks')
   , port = 12435;
 
 var AssetManager = require('../lib/assets').AssetManager
@@ -10,6 +11,14 @@ var AssetManager = require('../lib/assets').AssetManager
 function mocks(callback) {
     var app = express()
       , port_ = port++;
+    app.use(function (request, response, next) {
+        response.render = function (template) {
+            response.send(nunjucks.renderString(template, {
+                asset: response.locals.asset
+            }));
+        };
+        next();
+    });
     var server = app.listen(port_);
     function request(uri, callback) {
         http.request({
@@ -56,6 +65,21 @@ describe('Middleware', function () {
           , middleware = manager.middleware();
         middleware._id_ = 'foo';
         assert.equal(manager.middleware()._id_, 'foo');
+    });
+
+    it('should provide a view helper for defining assets', function (done) {
+        var manager = new AssetManager(path.join(fixtures, 'empty'));
+        mocks(function (app, request, next) {
+            app.use(manager.middleware());
+            app.get('/', function (request, response) {
+                response.render('{{ asset("foo.css") }}');
+            });
+            request('/', function (err, response, body) {
+                assert.ifError(err);
+                assert.equal(response.statusCode, 200);
+                next(done);
+            });
+        });
     });
 
 });
